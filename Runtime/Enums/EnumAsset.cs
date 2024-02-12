d1ef59a89e16b22289bee98ac460732cdb92bd58
John Baracuda <58704151+JohnBaracuda@users.noreply.github.com>
2024-02-12 16:40:23 +0100

    Inertia Update

--- Runtime/Enums/EnumAsset.cs
+++ Runtime/Enums/EnumAsset.cs
@@ -1,13 +1,9 @@
-using MobX.Mediator.Callbacks;
-using Sirenix.OdinInspector;
-using UnityEngine;
+using MobX.Mediator.Registry;
 
 namespace MobX.Mediator.Enums
 {
-    public abstract class EnumAsset<T> : ScriptableAsset where T : EnumAsset<T>
+    public abstract class EnumAsset<T> : RegisteredAsset where T : EnumAsset<T>
     {
-        [ReadOnly] [SerializeField] private int hash;
-
         public static T None => none ??= CreateInstance<T>();
 
         private static T none;
